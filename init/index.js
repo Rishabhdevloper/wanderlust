7c5b3c134fdc1d68e27b26692ac54f8db28bda0d
RishabhDevloper <rishabhsoni252003@gmail.com>
2025-07-28 16:57:40 +0530

    Fixed Routes

--- init/index.js
+++ init/index.js
@@ -2,26 +2,33 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const MONGO_URL = process.env.ATLAS_URL;
+// Load environment variables
+// require("dotenv").config();
 
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// const MONGO_URL = process.env.ATLAS_URL;
+const MONGO_URL="mongodb+srv://gomsimehani:mehanisonisax@cluster0.mjwqkzj.mongodb.net/"
+
+// console.log("MONGO_URL:", MONGO_URL);
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("connected to DB");
+
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({
+      ...obj,
+      owner: "6531350a2543406980779aae",
+    }));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+
+    await mongoose.disconnect();
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 }
 
-const initDB = async () => {
-  await Listing.deleteMany({});
-  initData.data = initData.data.map((obj) => ({...obj, owner:"6531350a2543406980779aae"}));
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
-};
-
-initDB();
+main();
